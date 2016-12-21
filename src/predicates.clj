fcd2cf4cc2275ac81f0fb6f6a2f8faf5952f6622
Otto Nascarella <ottoln@gmail.com>
2016-12-21 12:26:47 -0200

    finished chapter

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,51 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (= (float n) (float k))))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (every? whitespace? string))
 
 (defn has-award? [book award]
-  :-)
+  (let [awards (:awards book)]
+    (if (= nil awards)
+      false
+      (boolean (award awards)))))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (every? #(has-award? book %1) awards))
 
 (defn my-some [pred a-seq]
-  :-)
+  ;;(first (filter #(boolean %1) (map pred a-seq))))
+  (->> a-seq
+      (map pred)
+      (filter #(boolean %1))
+      (first)))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (= (count (filter pred a-seq)) (count a-seq)))
 
 (defn prime? [n]
-  :-)
-;^^
+  (let [pred #(= (mod n %1) 0)]
+    ;;(not (some pred (range 2 n)))))
+    (->> (range 2 n)
+        (some pred)
+        (not))))
